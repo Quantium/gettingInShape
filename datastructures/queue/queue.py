4fccc5e248c24e6c281f25d4d75ee5bd9d982570
Quantium <andres@quantium.com.mx>
2020-10-22 10:46:37 -0500

    Adding Queues

---
+++ datastructures/queue/queue.py
@@ -0,0 +1,33 @@
+#!usr/bin/python3
+"""
+FIFO
+"""
+class Queue:
+    def __init__(self):
+        self.queue=list()
+    
+    def enqueue(self,data):
+        if data not in self.queue:
+            self.queue.insert(0,data)
+            return True
+        return False
+    
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop()
+        return None
+    
+    def size(self):
+        return len(self.queue)
+
+aQueue = Queue()
+aQueue.enqueue("Andrés")
+aQueue.enqueue("Andy")
+aQueue.enqueue("Quantium")
+aQueue.enqueue("Qython")
+aQueue.enqueue("Chimi")
+print(aQueue.size())
+print("first element out",aQueue.dequeue())
+print("first element out",aQueue.dequeue())
+print("first element out",aQueue.dequeue())
+
